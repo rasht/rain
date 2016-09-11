f8f92bea9ea41e62327c9978f20a9d7142da1794
HatefRad <HatefRad@gmail.com>
2016-09-11 19:18:59 +0200

    managing the files

--- cool/client/app/coolCtrl.js
+++
@@ -1,8 +0,0 @@
-(function(angular) {
-  'use strict'
-  angular.module("cool", [])
-  	.controller('coolCtrl', function ($scope) {
-  		var vm = this;
-      	vm.greetings = "Hellooo";
-  });
-})(window.angular);
